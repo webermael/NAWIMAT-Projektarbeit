58b7a6e90cffdd76ef28c0d8b1cc4e48fae850f9
graberyanis <yanisgraber006@gmail.com>
2024-07-03 21:11:37 +0200

    added screen initialization function

--- src/utils/visualisation.py
+++ src/utils/visualisation.py
@@ -1,5 +1,13 @@
 import pygame
 
+def screen_init(config):
+    pygame.init()
+    size = (config.tile_height * config.column_length, config.tile_width * config.row_length)
+    screen = pygame.display.set_mode(size)
+    return screen
+
+
+# prints a value for every tile in the world
 def render_world(world):
     for row in world.grid:
         for field in row:
