fe63419dc5449b0b86a35df19d6f6be894919a6a
webermael <webermael7@gmail.com>
2024-07-03 21:47:07 +0200

    cleaned visualisation (added screen_update())

--- src/utils/visualisation.py
+++ src/utils/visualisation.py
@@ -1,11 +1,14 @@
 import pygame
 
 def screen_init(config):
-    pygame.init()
     size = (config.tile_height * config.column_length, config.tile_width * config.row_length)
     screen = pygame.display.set_mode(size)
+    screen.fill((255, 255, 255))
     return screen
 
+def screen_update():
+    pygame.display.flip()
+    # ...
 
 # prints a value for every tile in the world
 def render_world(world):
