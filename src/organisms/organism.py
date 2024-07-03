e300f8bf1c6dd8e0c239d5d0e323435eb131e7bb
graberyanis <yanisgraber006@gmail.com>
2024-07-03 11:08:19 +0200

    added lifetime system

--- src/organisms/organism.py
+++ src/organisms/organism.py
@@ -1,14 +1,29 @@
 #from src.organisms.sensors import Sensor
 #from src.organisms.neural_network import NeuralNetwork
+from world.tile import Position
 
 class Organism:
-    def __init__(self, config, ):
-        self.position = (0, 0)
-        self.vitality = 100 # current state of wellbeing, used to reduce lifetime more/less
-        self.lifetime = 80 # how long the organism will live
+    def __init__(self, config, x_pos, y_pos):
+        self.position = Position(x_pos, y_pos)
+        self.vitality = config.organism_vitality # current state of wellbeing, used to reduce lifetime more/less
+        self.lifetime = config.organism_lifetime # how long the organism will live
         self.alive = True
         #self.nn = NeuralNetwork(config)
 
-    def move(self, world):
-        pass
+    def move(self, world, direction):
+        self.position.x += direction[0]
+        self.position.y += direction[1]
 
+    def update_lifetime(self):
+        self.lifetime -= 100 / self.vitality
+        if self.lifetime <= 0:
+            self.alive = False
+    
+    def checktile(self, world):
+        tile_type = world[self.position.y][self.position.x].type
+        if tile_type == "empty":
+            ...
+        elif tile_type == "food":
+            ...
+        elif tile_type == "danger":
+            ...
