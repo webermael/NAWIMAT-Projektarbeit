1349222eb360780376dd6ba0f3bfd4a7e198e89b
graberyanis <yanisgraber006@gmail.com>
2024-07-03 09:08:58 +0200

    added attributes to Organism

--- src/organisms/organism.py
+++ src/organisms/organism.py
@@ -4,7 +4,9 @@
 class Organism:
     def __init__(self, config, ):
         self.position = (0, 0)
-        self.health = 100
+        self.vitality = 100 # current state of wellbeing, used to reduce lifetime more/less
+        self.lifetime = 80 # how long the organism will live
+        self.alive = True
         #self.nn = NeuralNetwork(config)
 
     def move(self, world):
