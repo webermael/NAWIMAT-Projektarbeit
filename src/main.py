f08ed9750d58c1a1d744e9775fd21698eff501da
graberyanis <yanisgraber006@gmail.com>
2024-07-03 21:12:21 +0200

    added a screen

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pygame
 from config import Config
 from world.world import World
 #from organisms.organism import Organism
-from utils.visualisation import render_world
+from utils.visualisation import (render_world, screen_init) 
 #from evolution.reproduction import reproduce
 #from evolution.mutation import mutate
 
@@ -12,9 +12,11 @@ def main():
     world = World(config)
     #print(world.grid[0][5].position)
     #organisms = [Organism(config) for _ in range(config.num_organisms)]
-    
+    screen = screen_init(config)
     while True:
         render_world(world)
+        screen.fill((255, 255, 255))
+        pygame.display.flip()
         break
 
 if __name__ == "__main__":
