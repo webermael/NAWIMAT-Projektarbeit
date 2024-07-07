e006c38873052a0c544a69b6af833f4229b8d2e9
WanderJohnie <156407185+WanderJohnie@users.noreply.github.com>
2024-07-07 12:00:08 +0200

    Created environment.py

---
+++ src/world/environment.py
@@ -0,0 +1,10 @@
+from world.tile import Tile
+
+class Berries(Tile):
+  def __init__(self):
+    self.content = "berries"
+
+class Fire(Tile):
+  def __init__(self):
+    self.content = "fire"
+    
