79ec1503af1a17be3d966ce0933219500e5af5b0
graberyanis <yanisgraber006@gmail.com>
2024-07-11 23:26:26 +0200

    changed default tiles to empty ones and added test danger tiles

--- src/world/world.py
+++ src/world/world.py
@@ -1,12 +1,16 @@
 from world.tile import Tile
-from world.environment import Danger
+from world.environment import Danger, Empty
 from random import randint
 
 class World:
     def __init__(self, config):
         self.width = config.row_length
         self.height = config.column_length
-        self.grid = [[Tile(config, x_pos, y_pos) for x_pos in range(self.width)] for y_pos in range(self.height)]
+        self.grid = [[Empty(config, x_pos, y_pos) for x_pos in range(self.width)] for y_pos in range(self.height)]
+        for i in range(15):
+            y = randint(0, config.column_length - 1)
+            x = randint(0, config.row_length - 1)
+            self.grid[y][x] = Danger(config, x, y)
     
     def draw(self, config, display):
         for column in self.grid:
