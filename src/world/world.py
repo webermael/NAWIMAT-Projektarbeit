1400ca28b1c5e18153b7161dbb430f2a7ef9b253
graberyanis <yanisgraber006@gmail.com>
2024-07-11 23:24:11 +0200

    added functions to draw all tiles and call spread function on all danger tiles

--- src/world/world.py
+++ src/world/world.py
@@ -1,7 +1,20 @@
 from world.tile import Tile
+from world.environment import Danger
+from random import randint
 
 class World:
     def __init__(self, config):
         self.width = config.row_length
         self.height = config.column_length
         self.grid = [[Tile(config, x_pos, y_pos) for x_pos in range(self.width)] for y_pos in range(self.height)]
+    
+    def draw(self, config, display):
+        for column in self.grid:
+            for tile in column:
+                tile.draw(config, display)
+    
+    def spread(self, config):
+        for column in self.grid:
+            for tile in column:
+                if tile.content == "danger":
+                    tile.spread(self.grid, config)
