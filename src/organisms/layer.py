bb79dfbcf1e9b31435d29ea953ba5d1d639df442
graberyanis <yanisgraber006@gmail.com>
2024-07-21 22:00:27 +0200

    added layer class used to create a network

---
+++ src/organisms/layer.py
@@ -0,0 +1,19 @@
+class Layer:
+    def __init__(self, nodes_in, size):
+        self.size = size
+        self.nodes_in = nodes_in # number of nodes in the previous layer
+        self.weights = [[1 for node in range(nodes_in)] for i in range(size)] # every node has a list of weights for all ingiong nodes
+        self.biases = [0 for node in range(size)] # bias for every node in the layer
+    
+
+    def calc_output(self, inputs):
+        outputs = []
+
+        for node in range(self.size):
+            output = self.biases[node] # for each node in the current layer, adds its bias first then the weighted input of each ingoing node 
+            for input in range(self.nodes_in):
+                output += self.weights[node][input] * inputs[input] # multiplies value of each ingoing node with its weight, repeated for each node in the previous layer
+
+            outputs.append(output)
+        
+        return outputs
