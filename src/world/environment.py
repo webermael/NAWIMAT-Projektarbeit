8b3f666d2ef3183168afa93394b567e57290635e
graberyanis <yanisgraber006@gmail.com>
2024-07-23 22:48:22 +0200

    added food to be drawn as green squares

--- src/world/environment.py
+++ src/world/environment.py
@@ -5,6 +5,10 @@ import pygame
 class Food(Tile):
   def __init__(self, config, x_pos, y_pos):
     Tile.__init__(self, config, x_pos, y_pos, "food")
+  
+  def draw(self, config, display):
+    pygame.draw.rect(display, (0, 255, 0), (self.position.x * config.tile_width, self.position.y * config.tile_width, config.tile_width, config.tile_width))
+
 
 class Danger(Tile):
   def __init__(self, config, x_pos, y_pos):
@@ -20,6 +24,7 @@ class Danger(Tile):
         if i >= 0 and i < config.column_length and j >= 0 and j < config.row_length:
           if randint(0, 100) == 0:
             world[i][j] = Danger(config, j, i)
+
 
 class Empty(Tile):
   def __init__(self, config, x_pos, y_pos):
