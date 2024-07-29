0498fbd8f256a9fa5c79812a1380693f846c77de
graberyanis <yanisgraber006@gmail.com>
2024-07-29 23:27:43 +0200

    added sigmoid activation function to nodes output

--- src/organisms/layer.py
+++ src/organisms/layer.py
@@ -1,9 +1,14 @@
+import math
 class Layer:
     def __init__(self, nodes_in, size):
         self.size = size
         self.nodes_in = nodes_in # number of nodes in the previous layer
         self.weights = [[1 for node in range(nodes_in)] for i in range(size)] # every node has a list of weights for all ingiong nodes
         self.biases = [0 for node in range(size)] # bias for every node in the layer
+    
+
+    def ActivationFunction(x):
+        return 1 / (1 + (math.e ** x))
     
 
     def calc_output(self, inputs):
@@ -14,6 +19,6 @@ class Layer:
             for input in range(self.nodes_in):
                 output += self.weights[node][input] * inputs[input] # multiplies value of each ingoing node with its weight, repeated for each node in the previous layer
 
-            outputs.append(output)
+            outputs.append(self.ActivationFunction(output))
         
-        return outputs
+        return outputs	
