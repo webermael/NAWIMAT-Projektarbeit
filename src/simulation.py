368c5652ca79f1b2fa5f9d84e88a9c3fd4fa9845
graberyanis <yanisgraber006@gmail.com>
2024-08-14 15:34:26 +0200

    created simulation class that has the same functionality as main

--- src/simulation.py
+++ src/simulation.py
@@ -1,3 +1,31 @@
+import pygame
+from config import Config
+from world.world import World
+from organisms.population import Population
+from utils.visualisation import (render_world, screen_init, screen_update)
+
+
 class Simulation():
     def __init__(self):
-        pass
+        self.config = Config()
+        self.world = World(self.config)
+        self.population = Population(self.config)
+        self.screen = screen_init(self.config)
+        self.running = True
+    
+    def run(self):
+        pygame.init()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: 
+                    self.running = False
+            self.screen.fill((255, 255, 255))
+
+            self.world.spread(self.config)
+            self.world.draw(self.config, self.screen)
+            self.population.update(self.config, self.world.grid)
+            self.population.draw(self.config, self.screen)
+            # drawing everything
+
+            screen_update()
+            pygame.time.Clock().tick(60)
