a8a0ad749a52f7838d30946d0ac54bd96d23fb61
Yanis Graber <161033947+graberyanis@users.noreply.github.com>
2024-08-14 15:42:35 +0200

    Add simulation class (#11)
    * added simulation file and class
    
    * created simulation class that has the same functionality as main
    
    * replaced main code with code from simulation class

--- src/main.py
+++ src/main.py
@@ -1,33 +1,15 @@
-import pygame
-from config import Config
-from world.world import World
-from organisms.population import Population
-from utils.visualisation import (render_world, screen_init, screen_update)
+#import pygame
+#from config import Config
+#from world.world import World
+#from organisms.population import Population
+#from utils.visualisation import (render_world, screen_init, screen_update)
 #from evolution.reproduction import reproduce
 #from evolution.mutation import mutate
+from simulation import Simulation
 
 def main():
-    pygame.init()
-    config = Config()
-    world = World(config)
-    population = Population(config)
-    screen = screen_init(config)
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
-                running = False
-        screen.fill((255, 255, 255))
-
-        world.draw(config, screen)
-        world.spread(config)
-        population.draw(config, screen)
-        population.update(config, world.grid)
-        # drawing everything
-
-        screen_update()
-        pygame.time.Clock().tick(60)
+    simulation = Simulation()
+    simulation.run()
 
 if __name__ == "__main__":
     main()
