cf4350c85ec16789c095a71d1fdb50f51689a3b7
graberyanis <yanisgraber006@gmail.com>
2024-08-14 20:59:33 +0200

    added functions to chose well performing organisms and create a new generation with them

--- src/simulation.py
+++ src/simulation.py
@@ -1,18 +1,23 @@
 import pygame
+import random
 from config import Config
 from world.world import World
 from organisms.population import Population
+from organisms.organism import Organism
+from evolution.reproduction import reproduction
 from utils.visualisation import (render_world, screen_init, screen_update)
 
 
 class Simulation():
     def __init__(self):
         self.config = Config()
-        self.reset()
+        self.generation_duration = self.config.generation_duration
+        self.world = World(self.config)
+        self.population = Population(self.config)
+        self.screen = screen_init(self.config)
         self.running = False
-    
+
     def run(self):
-        self.running = True
         pygame.init()
         while self.generation_duration > 0 and self.running:
             for event in pygame.event.get():
@@ -31,13 +36,53 @@ class Simulation():
             pygame.time.Clock().tick(60)
         pygame.quit()
     
+
+    def normalize_lifetime(self):
+        total = 0
+        for organism in self.population.organisms: # calculates the total "score" of all organisms
+            total += organism.lifetime
+        for organism in self.population.organisms:
+            organism.lifetime = organism.lifetime / total # divides all lifetimes so they all add up to 1
+
+
+    def selection(self, organisms):
+        start = random.random()
+        index = 0
+        while start > 0:
+            start -= organisms[index].lifetime 
+            index += 1
+        return organisms[index - 1].nn
+    '''
+    The selection function picks a random number from 0 to 1, the lifetime of every organism combined is 1 after being normalized by the noramlize lifetime function
+    If the start value was 1, you could subtract every lifetime and the last organism in the list, would finall bring it to 0
+    This way, any score/lifetime, that is higher than another gets a bigger chance of being selected:
+    Imagine one organism having a score of 0.9 (out of 1), so the rest of the organisms would all add up to 0.1
+    If the starting value is lower than 0.9, this organism would always be selected, 
+    as the other ones could only bring the value down to 0.8 and the strong organism would always get it to 0 or lower
+    '''
+    
+
+    def new_gen(self):
+        new_population = Population(self.config)
+        self.normalize_lifetime()
+
+        for organism in range(self.config.population_size):
+            new_org = Organism(self.config, 
+                               new_population.organisms[organism].position.x, 
+                               new_population.organisms[organism].position.y, 
+                               reproduction(self.config, self.selection(self.population.organisms), self.selection(self.population.organisms)))
+            new_population.organisms[organism] = new_org
+        return new_population
+
+
     def reset(self):
         self.generation_duration = self.config.generation_duration
         self.world = World(self.config)
-        self.population = Population(self.config)
+        self.population = self.new_gen()
         self.screen = screen_init(self.config)
     
     def evolve(self):
+        self.running = True
         while self.running:
             self.run()
             self.reset()
