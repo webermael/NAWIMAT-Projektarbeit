ca792923141e956feb74b6f95e88cae7dd5bc911
webermael <webermael7@gmail.com>
2024-08-15 14:05:07 +0200

    changed config values

--- src/config.py
+++ src/config.py
@@ -11,7 +11,7 @@ class Config:
 
         self.tickspeed = 60
 
-        self.danger_tiles = 0 #20
+        self.danger_tiles = 20
         self.food_tiles = 900
 
         self.danger_min_life = 15
@@ -21,16 +21,16 @@ class Config:
 
         self.food_min_spread = 60
         self.food_max_spread = 100
-        self.food_spread_chance = 5 # final chance wile be 1 / spread_chance
+        self.food_spread_chance = 2 # final chance wile be 1 / spread_chance
         self.food_bonus = 42
 
         self.population_size = 300 
 
         self.organism_eyes_size = 2
 
-        self.nn_layer_sizes = ((self.organism_eyes_size * 2 + 1) ** 2, 5, 9)
+        self.nn_layer_sizes = ((self.organism_eyes_size * 2 + 1) ** 2, 10, 5, 9)
 
         self.organism_start_life = 150
         self.organism_tick_damage = 1
         self.border_damage = 100
-        self.move_bonus = 1
+        self.move_bonus = 5
