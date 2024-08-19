ac2269ca671777f317655e28c212da3c570f3a14
graberyanis <yanisgraber006@gmail.com>
2024-08-19 20:53:16 +0200

    fixed double equals signs on assignments

--- src/evolution/reproduction.py
+++ src/evolution/reproduction.py
@@ -6,23 +6,23 @@ from organisms.neural_network import NeuralNetwork
  
 def reproduction(config, nn1, nn2):
     nn = NeuralNetwork(config.nn_layer_sizes)
-    for layer in range(len(nn1.layers)):
+    for layer in range(len(nn.layers)):
         current_layer = Layer(nn.layers[layer].nodes_in, nn.layers[layer].size)
 
         for weights in range(len(nn.layers[layer].weights)):
             choice = randint(1, 2)
             for node_weight in range(len(nn.layers[layer].weights[weights])):
                 if choice == 1:
-                    current_layer.weights[weights][node_weight] == mutation(config, nn1.layers[layer].weights[weights][node_weight])
+                    current_layer.weights[weights][node_weight] = mutation(config, nn1.layers[layer].weights[weights][node_weight])
                 elif choice == 2:
-                    current_layer.weights[weights][node_weight] == mutation(config, nn2.layers[layer].weights[weights][node_weight])
+                    current_layer.weights[weights][node_weight] = mutation(config, nn2.layers[layer].weights[weights][node_weight])
 
-        for bias in range(len(nn1.layers[layer].biases)):
+        for bias in range(len(nn.layers[layer].biases)):
             choice = randint(1, 2)
             if choice == 1:
-                current_layer.biases[bias] == mutation(config, nn1.layers[layer].biases[bias])
+                current_layer.biases[bias] = mutation(config, nn1.layers[layer].biases[bias])
             elif choice == 2:
-                current_layer.biases[bias] == mutation(config, nn2.layers[layer].biases[bias])
+                current_layer.biases[bias] = mutation(config, nn2.layers[layer].biases[bias])
 
         nn.layers[layer] = current_layer
     return nn
