1be28295c15d79a3a6f6d9b2ebc5ba8dc380c046
graberyanis <yanisgraber006@gmail.com>
2024-08-19 20:54:08 +0200

    changed mutation to use addition instead of multplication

--- src/evolution/mutation.py
+++ src/evolution/mutation.py
@@ -1,6 +1,6 @@
 import random
 def mutation(config, value):
     if random.random() <= config.mutation_rate:
-        return value * random.uniform(config.mutation_min_multiplier, config.mutation_max_multiplier)
+        return value + random.uniform(config.mutation_max_difference, -config.mutation_max_difference)
     else:
         return value
