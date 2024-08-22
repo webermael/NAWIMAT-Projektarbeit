95ac0e6432156fb6536cd046a3e333e65438151d
Maximilian Bail <156407185+WanderJohnie@users.noreply.github.com>
2024-08-22 21:21:56 +0200

    added temporary title and geometry to GUI

--- src/utils/GUI.py
+++ src/utils/GUI.py
@@ -3,5 +3,8 @@ import tkinter
 class GUI:
   def __init__(self):
     self.root = tkinter.TK()
-
-
+    self.title = root.title("Simulation") #Title should prly be changed
+    self.geometry = root.geometry("500x500") 
+    
+    root.mainloop()
+    
