04034c29f6dee8ddb255a7a85551671631a1e228
Maximilian Bail <156407185+WanderJohnie@users.noreply.github.com>
2024-08-23 10:48:06 +0200

    Update GUI.py

--- src/utils/GUI.py
+++ src/utils/GUI.py
@@ -1,10 +1,12 @@
 import tkinter
 
 class GUI:
-  def __init__(self):
+  def __init__(self, config):
     self.root = tkinter.TK()
-    self.title = root.title("Simulation") #Title should prly be changed
-    self.geometry = root.geometry("500x500") 
-    
+    self.title = "Simulation"
+    root.title(self.title)  #Title should prly be changed
+    self.geometry = (f"{config.tile_width}x{config.tile_length}") 
+    root.geometry = (self.geometry)
+
     root.mainloop()
     
