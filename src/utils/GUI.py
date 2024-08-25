565bcba9eb7a66b123030d8555cc1c5c2f645305
Maximilian Bail <156407185+WanderJohnie@users.noreply.github.com>
2024-08-25 22:04:41 +0200

    added button1 and label1 to GUI.py

--- src/utils/GUI.py
+++ src/utils/GUI.py
@@ -3,10 +3,22 @@ import tkinter
 class GUI:
   def __init__(self, config):
     self.root = tkinter.TK()
-    self.title = "Simulation"
-    root.title(self.title)  #Title should prly be changed
+    
+    self.title = "Simulation" #Title should prly be changed
+    self.root.title(self.title)
+    
     self.geometry = (f"{config.tile_width}x{config.tile_length}") 
-    root.geometry = (self.geometry)
+    self.root.geometry = (self.geometry)
 
-    root.mainloop()
+    self.label1 = tkinter.Label(self.root, text = "SimEvolator", font = ('Arial')) # Isn't really nessecary
+    self.label1.place(relx = 0.2, rely = 0.1, relwidth = 0.6)
+
+    self.button1 = tkinter.Button(self.root, text = "Start!", font = ('Arial'), command = start_sim)
+    self.button1.place(relx = 0.4, rely = 0.6, relwidth = 0.4, relheigt = 0.247) # relheight = golden section from relwidth
+    
+    self.root.mainloop()
+    def start_sim():
+      #start the simulation
+      self.root.destroy() # closes the root window 
+    
     
