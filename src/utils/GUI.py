48e8771ba1378805d89a988d26c0b2d88817798d
Maximilian Bail <156407185+WanderJohnie@users.noreply.github.com>
2024-08-26 22:07:53 +0200

    Added class Startpage/Settings , added scales

--- src/utils/GUI.py
+++ src/utils/GUI.py
@@ -1,24 +1,69 @@
 import tkinter
 
-class GUI:
+class Startpage:
   def __init__(self, config):
-    self.root = tkinter.TK()
+    self.startpage = tkinter.TK() 
     
-    self.title = "Simulation" #Title should prly be changed
-    self.root.title(self.title)
+    self.title = "Simulation" 
+    self.startpage.title(self.title) 
     
+    self.geometry = ("800x600") 
+    self.startpage.geometry = (self.geometry)
+    self.startpage.resizable(width = False, heigth = False)
+    
+
+    self.label1 = tkinter.Label(self.startpage, text = "SimEvolator", font = ('Arial'))  
+    self.label1.place(relx = 0.2, rely = 0.1, relwidth = 0.6) 
+
+    self.button1 = tkinter.Button(self.startpage, text = "Start!", font = ('Arial'), command = self.start)
+    self.button1.place(relx = 0.4, rely = 0.6, relwidth = 0.4, relheigt = 0.247) 
+    
+    self.startpage.mainloop()
+    def start (self):
+      self.startpage.destroy()
+      Settings()
+
+
+class Settings:
+  def __init__(self, config):
+    self.settings = tkinter.TK()
+
+    self.title = "Simulation"
+    self.settings.title(self.title)
+
     self.geometry = (f"{config.tile_width}x{config.tile_length}") 
-    self.root.geometry = (self.geometry)
+    self.settings.geometry = (self.geometry)
 
-    self.label1 = tkinter.Label(self.root, text = "SimEvolator", font = ('Arial')) # Isn't really nessecary
-    self.label1.place(relx = 0.2, rely = 0.1, relwidth = 0.6)
+    self.scaleWidth = 50
+    self.scaleLength = 300
+    self.scaleLow = 1
+    self.scaleHigh = 5000
+    
+    self.scaleRowLength = tkinter.Scale(self.settings, from_= self.scaleLow, to = self.scaleHigh, width = self.scaleWidth, length = self.scaleLength) 
+    #self.scaleRowLength.place(x = 60 , y = 75)
+    
+    self.scaleColumnLength = tkinter.Scale(self.settings, from_= self.scaleLow, to = self.scaleHigh, width = self.scaleWidth, length = self.scaleLength) 
+    #self.scaleRowLength.place(x = 210 , y = 75)
 
-    self.button1 = tkinter.Button(self.root, text = "Start!", font = ('Arial'), command = start_sim)
-    self.button1.place(relx = 0.4, rely = 0.6, relwidth = 0.4, relheigt = 0.247) # relheight = golden section from relwidth
+    self.scalePopulation = tkinter.Scale(self.settings, from_= self.scaleLow, to = self.scaleHigh, width = self.scaleWidth, length = self.scaleLength) 
+    #self.scaleRowLength.place(x = 360 , y = 75)
+
+    self.scaleGeneration = tkinter.Scale(self.settings, from_= self.scaleLow, to = self.scaleHigh, width = self.scaleWidth, length = self.scaleLength) 
+    #self.scaleRowLength.place(x = 510 , y = 75)
+
+    self.buttonStart = tkinter.Button(self.settings, text = "Start!", command = )
+    #self.buttonStart.place()
+
+def getScaleValue(self):
+  value1 = self.scaleRowlength.get() # Var names are all wrong
+  value2 = self.scaleColumnLenght.get()
+  value3 = self.scalePopulation.get()
+  value4 = self.scaleGeneration.get()
+  self.settings.destroy()    #closes the window
+  #start the sim
+  
+  
     
-    self.root.mainloop()
-    def start_sim():
-      #start the simulation
-      self.root.destroy() # closes the root window 
+
     
-    
+        
