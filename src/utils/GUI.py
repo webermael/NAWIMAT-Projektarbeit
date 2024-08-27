baa031645c4497282619fcd70708e7cd62d20efd
Maximilian Bail <156407185+WanderJohnie@users.noreply.github.com>
2024-08-27 21:56:11 +0200

    Added class Scale  and started on scrollbar

--- src/utils/GUI.py
+++ src/utils/GUI.py
@@ -1,8 +1,9 @@
 import tkinter
+from tkinter import ttk
 
 class Startpage:
   def __init__(self, config):
-    self.startpage = tkinter.TK() 
+    self.startpage = tkinter.Tk() 
     
     self.title = "Simulation" 
     self.startpage.title(self.title) 
@@ -15,7 +16,7 @@ class Startpage:
     self.label1 = tkinter.Label(self.startpage, text = "SimEvolator", font = ('Arial'))  
     self.label1.place(relx = 0.2, rely = 0.1, relwidth = 0.6) 
 
-    self.button1 = tkinter.Button(self.startpage, text = "Start!", font = ('Arial'), command = self.start)
+    self.button1 = tkinter.ttk.Button(self.startpage, text = "Start!", font = ('Arial'), command = self.start)
     self.button1.place(relx = 0.4, rely = 0.6, relwidth = 0.4, relheigt = 0.247) 
     
     self.startpage.mainloop()
@@ -23,39 +24,37 @@ class Startpage:
       self.startpage.destroy()
       Settings()
 
-
+class Scale:
+  def __init__(self, root, scaleLow, scaleHigh):
+    self.scale = tkinter.ttk.Scale(root, from_ = scaleLow, to = scaleHigh)
+    
 class Settings:
   def __init__(self, config):
-    self.settings = tkinter.TK()
+    self.settings = tkinter.Tk()
 
     self.title = "Simulation"
     self.settings.title(self.title)
 
-    self.geometry = (f"{config.tile_width}x{config.tile_length}") 
+    self.geometry = ("800x600") 
     self.settings.geometry = (self.geometry)
 
-    self.scaleWidth = 50
-    self.scaleLength = 300
-    self.scaleLow = 1
-    self.scaleHigh = 5000
+    self.scaleRowLength = Scale(self.settings, 1, 5000)    
+    self.scaleColumnLength = Scale(self.settings, 1, 5000)
+    self.scalePopulation = Scale(self.settings, 10, 1000)
+    self.scaleGeneration = Scale(self.settings, 1, 1000)
     
-    self.scaleRowLength = tkinter.Scale(self.settings, from_= self.scaleLow, to = self.scaleHigh, width = self.scaleWidth, length = self.scaleLength) 
-    #self.scaleRowLength.place(x = 60 , y = 75)
+    self.scales = {self.scaleRowLength, self.scaleColumnLength, self.scalePopulation, self.scaleGeneration}# Prly last 4 lines are able to be directly here (not on my vs code)
+    self.scrollbar = tkinter.ttk.Scrollbar(self.settings, orient='vertical', command=self.scales.yview)# .yview is a problem yet
+    self.scrollbar.grid(row=0, column=1, sticky=tkinter.NS)
+    self.scales['yscrollcommand'] = self.scrollbar.set
+
+    self.settings.mainloop()
     
-    self.scaleColumnLength = tkinter.Scale(self.settings, from_= self.scaleLow, to = self.scaleHigh, width = self.scaleWidth, length = self.scaleLength) 
-    #self.scaleRowLength.place(x = 210 , y = 75)
 
-    self.scalePopulation = tkinter.Scale(self.settings, from_= self.scaleLow, to = self.scaleHigh, width = self.scaleWidth, length = self.scaleLength) 
-    #self.scaleRowLength.place(x = 360 , y = 75)
+    
 
-    self.scaleGeneration = tkinter.Scale(self.settings, from_= self.scaleLow, to = self.scaleHigh, width = self.scaleWidth, length = self.scaleLength) 
-    #self.scaleRowLength.place(x = 510 , y = 75)
-
-    self.buttonStart = tkinter.Button(self.settings, text = "Start!", command = )
-    #self.buttonStart.place()
-
-def getScaleValue(self):
-  value1 = self.scaleRowlength.get() # Var names are all wrong
+def getScaleValue(self):    #command for getting values from scales
+  value1 = self.scaleRowLength.get() # Var names are all wrong
   value2 = self.scaleColumnLenght.get()
   value3 = self.scalePopulation.get()
   value4 = self.scaleGeneration.get()
