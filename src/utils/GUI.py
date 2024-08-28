cbd8d0c279ee127723ab29bd098a4f10498ccc5f
Maximilian Bail <156407185+WanderJohnie@users.noreply.github.com>
2024-08-28 21:09:14 +0200

    Added class Config and updated class Scale

--- src/utils/GUI.py
+++ src/utils/GUI.py
@@ -3,66 +3,63 @@ from tkinter import ttk
 
 class Startpage:
   def __init__(self, config):
-    self.startpage = tkinter.Tk() 
-    
-    self.title = "Simulation" 
-    self.startpage.title(self.title) 
-    
-    self.geometry = ("800x600") 
-    self.startpage.geometry = (self.geometry)
-    self.startpage.resizable(width = False, heigth = False)
-    
+    #configs
+    self.startpage = tkinter.Tk()
+    Config(self.startpage)
 
-    self.label1 = tkinter.Label(self.startpage, text = "SimEvolator", font = ('Arial'))  
+    #widgets
+    self.label1 = tkinter.ttk.Label(self.startpage, text = "SimEvolator", font = ('Arial'))  
     self.label1.place(relx = 0.2, rely = 0.1, relwidth = 0.6) 
 
     self.button1 = tkinter.ttk.Button(self.startpage, text = "Start!", font = ('Arial'), command = self.start)
     self.button1.place(relx = 0.4, rely = 0.6, relwidth = 0.4, relheigt = 0.247) 
     
     self.startpage.mainloop()
+    
     def start (self):
-      self.startpage.destroy()
-      Settings()
-
-class Scale:
-  def __init__(self, root, scaleLow, scaleHigh):
-    self.scale = tkinter.ttk.Scale(root, from_ = scaleLow, to = scaleHigh)
+      self.startpage.destroy()# closes Startpage
+      Settings()# opens Settings
     
 class Settings:
   def __init__(self, config):
+    #configs
     self.settings = tkinter.Tk()
-
-    self.title = "Simulation"
-    self.settings.title(self.title)
-
-    self.geometry = ("800x600") 
-    self.settings.geometry = (self.geometry)
-
-    self.scaleRowLength = Scale(self.settings, 1, 5000)    
-    self.scaleColumnLength = Scale(self.settings, 1, 5000)
-    self.scalePopulation = Scale(self.settings, 10, 1000)
-    self.scaleGeneration = Scale(self.settings, 1, 1000)
+    Config(self.settings)
     
-    self.scales = {self.scaleRowLength, self.scaleColumnLength, self.scalePopulation, self.scaleGeneration}# Prly last 4 lines are able to be directly here (not on my vs code)
+    #widgets
+    self.scales = {
+      self.scaleRowLength = Scale(self.settings, 1, 5000)    
+      self.scaleColumnLength = Scale(self.settings, 1, 5000)
+      self.scalePopulation = Scale(self.settings, 10, 1000)
+      self.scaleGeneration = Scale(self.settings, 1, 1000)
+      }
+    
     self.scrollbar = tkinter.ttk.Scrollbar(self.settings, orient='vertical', command=self.scales.yview)# .yview is a problem yet
     self.scrollbar.grid(row=0, column=1, sticky=tkinter.NS)
     self.scales['yscrollcommand'] = self.scrollbar.set
 
     self.settings.mainloop()
     
+  def getScaleValue(self):# command for getting values from scales
+    value1 = self.scaleRowLength.get()# Var names are all wrong
+    value2 = self.scaleColumnLenght.get()
+    value3 = self.scalePopulation.get()
+    value4 = self.scaleGeneration.get()
+    self.settings.destroy()# closes the window
+    #start the sim
 
+class Config:# maybe the class name is problematic
+  def __init__(self,root):
+    # root = tkinter.Tk() and root.mainloop() have to be directly in the specific class
+    self.geometry = "800x600"
+    root.geometry(self.geometry)
+    root.resizable(width = False, heigth = False)
     
+    self.title = "Simulation"
+    root.title(self.title)
 
-def getScaleValue(self):    #command for getting values from scales
-  value1 = self.scaleRowLength.get() # Var names are all wrong
-  value2 = self.scaleColumnLenght.get()
-  value3 = self.scalePopulation.get()
-  value4 = self.scaleGeneration.get()
-  self.settings.destroy()    #closes the window
-  #start the sim
-  
-  
-    
-
-    
+class Scale:
+  def __init__(self, root, scaleLow, scaleHigh):
+    self.scale = tkinter.ttk.Scale(root, from_ = scaleLow, to = scaleHigh)
+    self.scale.pack(expand = True) # not really a solution
         
