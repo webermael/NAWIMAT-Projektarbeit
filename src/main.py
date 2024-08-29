c6eb94eb5166bc27b135026a6f32ad43f4c6a193
graberyanis <yanisgraber006@gmail.com>
2024-08-29 16:00:00 +0200

    creates a dictionary with all the networks values and the generation counter value

--- src/main.py
+++ src/main.py
@@ -6,10 +6,24 @@
 #from evolution.reproduction import reproduce
 #from evolution.mutation import mutate
 from simulation import Simulation
+from utils.file_manager import FileManager
 
 def main():
-    simulation = Simulation()
+    FM = FileManager()
+    simulation = Simulation(FM.load_file("input.json"))
     simulation.evolve()
+    save = {}
+    save["generation_counter"] = simulation.generation_counter
+    save["organisms"] = [{"network_weights" : 
+                            [layer.weights for layer in simulation.population.organisms[organism].nn.layers], 
+                            
+                            "network_biases" : 
+                            [layer.biases for layer in simulation.population.organisms[organism].nn.layers]} 
+                            
+                            for organism in range(len(simulation.population.organisms))
+                            ]
+
+    FM.save_dict("save.json", save)
 
 if __name__ == "__main__":
     main()
