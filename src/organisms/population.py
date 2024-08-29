8bf76a4f471e3a27f7ef19b88c80e9603523dc39
graberyanis <yanisgraber006@gmail.com>
2024-08-29 15:59:23 +0200

    if a save file dictionary is given, uses it to recreate the networks from the save

--- src/organisms/population.py
+++ src/organisms/population.py
@@ -1,14 +1,21 @@
 from organisms.organism import Organism
+from organisms.neural_network import NeuralNetwork
 from random import randint
 
 class Population:
-    def __init__(self, config):
+    def __init__(self, config, network_dict = False):
         self.organisms = []
         self.organism_coords = []
         while len(self.organisms) < config.population_size:
             coords = (randint(0, config.row_length - 1), randint(0, config.column_length - 1))
             if coords not in self.organism_coords:
-                self.organisms.append(Organism(config, coords[0], coords[1]))
+                if network_dict:
+                    nn = NeuralNetwork(config.nn_layer_sizes, 
+                                       network_dict["organisms"][len(self.organisms)]["network_weights"],
+                                       network_dict["organisms"][len(self.organisms)]["network_biases"])
+                    self.organisms.append(Organism(config, coords[0], coords[1], nn))
+                else:
+                    self.organisms.append(Organism(config, coords[0], coords[1]))
                 self.organism_coords.append(coords)
     
     def draw(self, config, display):
