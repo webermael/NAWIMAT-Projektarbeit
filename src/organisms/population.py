6347a0f14f9f6c375d0dc5c8345ba5c56cc78f4f
graberyanis <yanisgraber006@gmail.com>
2024-09-12 16:28:51 +0200

    changed input dictionary hierarchy

--- src/organisms/population.py
+++ src/organisms/population.py
@@ -3,16 +3,16 @@ from organisms.neural_network import NeuralNetwork
 from random import randint
 
 class Population:
-    def __init__(self, config, network_dict = False):
+    def __init__(self, config, input_dict):
         self.organisms = []
         self.organism_coords = []
         while len(self.organisms) < config.population_size:
             coords = (randint(0, config.row_length - 1), randint(0, config.column_length - 1))
             if coords not in self.organism_coords:
-                if network_dict:
+                if input_dict["organisms"]["networks"] != False:
                     nn = NeuralNetwork(config.nn_layer_sizes, 
-                                       network_dict["organisms"][len(self.organisms)]["network_weights"],
-                                       network_dict["organisms"][len(self.organisms)]["network_biases"])
+                                       input_dict["organisms"]["networks"][len(self.organisms)]["weights"],
+                                       input_dict["organisms"]["networks"][len(self.organisms)]["biases"])
                     self.organisms.append(Organism(config, coords[0], coords[1], nn))
                 else:
                     self.organisms.append(Organism(config, coords[0], coords[1]))
