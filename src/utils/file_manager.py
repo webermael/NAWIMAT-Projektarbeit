41af5cc110846710d6597cf6b82db2e5f78538bf
graberyanis <yanisgraber006@gmail.com>
2024-09-12 16:27:27 +0200

    updates the variables of the current save, which are changed by the simulation

--- src/utils/file_manager.py
+++ src/utils/file_manager.py
@@ -10,6 +10,19 @@ class FileManager:
         file.close()
         return file_content
     
+    def sim_to_dict(self, loaded_dict, simulation):
+        networks = [{
+                    "weights" : 
+                        [layer.weights for layer in simulation.population.organisms[organism].nn.layers], 
+                    "biases" : 
+                        [layer.biases for layer in simulation.population.organisms[organism].nn.layers]
+                    }
+                    for organism in range(len(simulation.population.organisms))
+                    ],
+        loaded_dict["population"]["organisms"]["networks"] = networks
+        loaded_dict["generation_counter"] = simulation.generation_counter
+        return loaded_dict
+    
     def save_dict(self, path, dict):
         with open(path, "w") as file:
             file = open(path, "w")
