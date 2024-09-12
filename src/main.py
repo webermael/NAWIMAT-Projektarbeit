89e4b0c8a7dd0d83ecb9f92d88c34af2f7c065ac
graberyanis <yanisgraber006@gmail.com>
2024-09-12 16:29:25 +0200

    now updates the output dictionary through file manager

--- src/main.py
+++ src/main.py
@@ -9,21 +9,14 @@ from simulation import Simulation
 from utils.file_manager import FileManager
 
 def main():
-    FM = FileManager()
-    simulation = Simulation(FM.load_file("input.json"))
-    simulation.evolve()
-    save = {}
-    save["generation_counter"] = simulation.generation_counter
-    save["organisms"] = [{"network_weights" : 
-                            [layer.weights for layer in simulation.population.organisms[organism].nn.layers], 
-                            
-                            "network_biases" : 
-                            [layer.biases for layer in simulation.population.organisms[organism].nn.layers]} 
-                            
-                            for organism in range(len(simulation.population.organisms))
-                            ]
+    fm = FileManager()
+    load_dict = fm.load_file("template.json")
+    simulation = Simulation(load_dict)
 
-    FM.save_dict("save.json", save)
+    simulation.evolve()
+    
+    dict = fm.sim_to_dict(load_dict, simulation)
+    fm.save_dict("save.json", dict)
 
 if __name__ == "__main__":
     main()
