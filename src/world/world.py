03deacbb597d4be38239e289b46a8e1b387f8da5
graberyanis <yanisgraber006@gmail.com>
2024-09-13 13:34:00 +0200

    world now generates strings, then replaces them with object as empty tiles are replaced by food and danger, this way there won't be unnecessary empty tile objects

--- src/world/world.py
+++ src/world/world.py
@@ -6,22 +6,31 @@ class World:
     def __init__(self, inputs):
         self.width = inputs["size"]["row_length"]
         self.height = inputs["size"]["column_length"]
-        self.grid = [[Empty(inputs["size"]["tile_width"], x_pos, y_pos) for x_pos in range(self.width)] for y_pos in range(self.height)]
+        self.grid = [["empty" for x_pos in range(self.width)] for y_pos in range(self.height)] 
 
         i = 0
         while i < inputs["danger"]["count"]:
             y = randint(0, self.height - 1)
             x = randint(0, self.width - 1)
-            if self.grid[y][x].content == "empty":
-                self.grid[y][x] = Danger(inputs, x, y)
+            if self.grid[y][x] == "empty":
+                self.grid[y][x] = "danger"
                 i += 1
         i = 0
         while i < inputs["food"]["count"]:
             y = randint(0, self.height - 1)
             x = randint(0, self.width - 1)
-            if self.grid[y][x].content == "empty":
-                self.grid[y][x] = Food(inputs, x, y)
+            if self.grid[y][x] == "empty":
+                self.grid[y][x] = "food"
                 i += 1
+
+        for row in range(len(self.grid)):
+            for tile in range(len(self.grid[0])):
+                if self.grid[row][tile] == "food":
+                    self.grid[row][tile] = Food(inputs, tile, row)
+                elif self.grid[row][tile] == "danger":
+                    self.grid[row][tile] = Danger(inputs, tile, row)
+                else:
+                    self.grid[row][tile] = Empty(inputs["size"]["tile_width"], tile, row)
     
     def draw(self, tile_width, display):
         for column in self.grid:
