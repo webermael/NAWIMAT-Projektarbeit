7f99a89228208717baef188b6db180787ce44a11
graberyanis <yanisgraber006@gmail.com>
2024-09-13 13:26:29 +0200

    changed "chance" to "probability"

--- src/evolution/mutation.py
+++ src/evolution/mutation.py
@@ -1,6 +1,6 @@
 import random
 def mutation(inputs, value):
-    if random.random() <= inputs["chance"]:
+    if random.random() <= inputs["probability"]:
         return value + random.uniform(inputs["max_difference"], -inputs["max_difference"])
     else:
         return value
