fcc3b1e4612712c7551c07216f1e6ecf05a75e04
webermael <webermael7@gmail.com>
2024-09-16 23:38:50 +0200

    added Slider class + (basic) SettingsWindow

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from simulation import Simulation
+from utils.gui import LoadWindow
+from utils.gui import SettingsWindow
 from utils.file_manager import FileManager
 import os.path
 
@@ -8,19 +10,22 @@ def main():
     my_path = os.path.abspath(os.path.dirname(__file__))
     path = os.path.join(my_path, "template.json")
 
-    load_dict = fm.load_file(path)
-
-    eyes_size = load_dict["population"]["organisms"]["eyes_size"]
-    load_dict["population"]["organisms"]["nn_layer_sizes"] = [2 * (eyes_size ** 2 + eyes_size) + 3, 
-                                                                (1 + eyes_size) * 4, 
-                                                                (1 + eyes_size) * 2, 
-                                                                9]
-    simulation = Simulation(load_dict)
-
-    simulation.evolve()
-    
-    dict = fm.sim_to_dict(load_dict, simulation)
-    fm.save_dict("save.json", dict)
+    while True:
+        # --- LOAD FILE ---
+        load_window = LoadWindow()
+        load_window.run()
+        load_dict = fm.load_file(load_window.loaded_file)
+        eyes_size = load_dict["population"]["organisms"]["eyes_size"]
+        load_dict["population"]["organisms"]["nn_layer_sizes"] = [2 * (eyes_size ** 2 + eyes_size) + 3, 
+                                                                    (1 + eyes_size) * 4, 
+                                                                    (1 + eyes_size) * 2, 
+                                                                    9]
+        settings_window = SettingsWindow()
+        settings_window.run()
+        simulation = Simulation(load_dict)
+        dict = fm.sim_to_dict(load_dict, simulation)
+        fm.save_dict("save.json", dict)
+            
 
 if __name__ == "__main__":
     main()
--- src/utils/gui.py
+++ src/utils/gui.py
@@ -1,13 +1,40 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 
-class Startpage:
+class Slider:
+  def __init__(self, value, root, scaleLow, scaleHigh):
+    self.frame = tk.Frame(root)
+    self.frame.columnconfigure(0, weight=1)
+    self.frame.columnconfigure(1, weight=3)
+    self.frame.columnconfigure(2, weight=1)
+    self.slider = tk.ttk.Scale(
+        self.frame,
+        from_=scaleLow,
+        to=scaleHigh,
+        orient="horizontal",
+        command=self.slider_changed,
+    )
+    self.slider.grid(column=1, row=0, sticky='we')
+    
+    self.label = ttk.Label(self.frame, text=f"{value}:")
+    self.label.grid(column=0, row=0, sticky='w')
+
+    self.value_label = ttk.Label(self.frame, text=self.get_current_value())
+    self.value_label.grid(column=2, row=0, sticky='e')
+    self.frame.pack(expand = True)
+
+  def get_current_value(self):
+      return '{: .2f}'.format(self.slider.get())
+
+  def slider_changed(self, event):
+      self.value_label.configure(text=self.get_current_value())
+
+class LoadWindow():
   def __init__(self):
-    # setup
     self.root = tk.Tk()
     self.root.title("Simulation")
     self.root.geometry("300x400")
-    # Config(self.root)
     self.root.columnconfigure(0, weight=1)
     self.root.columnconfigure(1, weight=3)
     self.root.columnconfigure(2, weight=1)
@@ -17,71 +44,106 @@ class Startpage:
     self.root.rowconfigure(3, weight=1)
     self.root.rowconfigure(4, weight=1)
 
-    self.lTitle = tk.ttk.Label(self.root, text = "SimEvolator")  
+    self.lTitle = tk.ttk.Label(self.root, text = "Load File")  
     self.lTitle.grid(column=1, row=1) 
 
-    self.bStart = tk.ttk.Button(self.root, text = "Start!", command = self.start)
+    self.bStart = tk.ttk.Button(self.root, text = "Use Template", command = self.template)
     self.bStart.grid(column=1, row=2)
 
-    self.bSettings = tk.ttk.Button(self.root, text = "Settings", command = self.settings)
-    self.bSettings.grid(column=1, row=3)
+    self.bStart = tk.ttk.Button(self.root, text = "Choose File", command = self.load_file)
+    self.bStart.grid(column=1, row=3)
+
+    self.loaded_file = ""
+
 
   def run(self):
     self.root.mainloop()
 
-  def start(self, simulation):
+  def template(self):
+    self.loaded_file = "template.json"
     self.root.destroy()
-    simulation.evolve()
+    
+  def load_file(self):
+    filetypes = [('JSON files', '*.json')]
 
-  def settings(self):
+    self.loaded_file = filedialog.askopenfilename(
+        title='Open a file',
+        initialdir='~/',
+        filetypes=filetypes)
     self.root.destroy()
-    Settings()
-
-  # widgets
     
-class Settings:
-  def __init__(self, config):
-    #configs
-    self.settings = tk.Tk()
-    Config(self.settings)
+
+class SettingsWindow():
+  def __init__(self):
+    # Window
+    self.root = tk.Tk()
+    self.root.title("Interface")
+    self.root.geometry("300x400")
+
+    # Layout setup
+    self.main_frame = tk.Frame(self.root)
+    self.main_frame.pack(fill="both", expand=1)
+
+    self.scroll_frame = tk.Frame(self.main_frame)
+    self.scroll_frame.pack(fill="y", side="right")
+
+    self.canvas = tk.Canvas(self.main_frame)
+    self.canvas.pack(side="left", fill="both", expand=1)
+
+    # Scrollbar
+    self.scrollbar = tk.ttk.Scrollbar(
+        self.scroll_frame,
+        orient="vertical",
+        command=self.canvas.yview
+    )
+    self.scrollbar.pack(side="right", fill="y")
+
+    # canvas config
+    self.canvas.configure(yscrollcommand=self.scrollbar.set)
+    self.canvas.bind("<Configure>",
+                lambda e: self.canvas.config(scrollregion=self.canvas.bbox("all"))) 
+
+    # frame inside canvas
+    self.content_frame = tk.Frame(self.canvas)
+
+    # add window -> content gets displayed
+    self.canvas.create_window((0, 0), window=self.content_frame, anchor="nw")
+
+    # self.slider = tk.ttk.Scale(
+    #     self.content_frame,
+    #     from_=0,
+    #     to=100,
+    #     orient="horizontal",
+    #     command=self.printValue
+    # )
+    # self.slider.pack()
+
+    slider1 = Slider("Slider 1", self.content_frame, 0, 100)
+
+  def printValue(self, event):
+      print(self.slider.get())
+
+  def run(self):
+    self.root.mainloop()
+# class Settings MAX:
+#     # self.scales = {
+#     #   self.scaleRowLength = Scale(self.settings, 1, 5000)    
+#     #   self.scaleColumnLength = Scale(self.settings, 1, 5000)
+#     #   self.scalePopulation = Scale(self.settings, 10, 1000)
+#     #   self.scaleGeneration = Scale(self.settings, 1, 1000)
+#     #   }
     
-    #widgets
-    # self.scales = {
-    #   self.scaleRowLength = Scale(self.settings, 1, 5000)    
-    #   self.scaleColumnLength = Scale(self.settings, 1, 5000)
-    #   self.scalePopulation = Scale(self.settings, 10, 1000)
-    #   self.scaleGeneration = Scale(self.settings, 1, 1000)
-    #   }
+#     self.scrollbar = tk.ttk.Scrollbar(self.settings, orient='vertical', command=self.scales.yview)# .yview is a problem yet
+#     self.scrollbar.grid(row=0, column=1, sticky=tk.NS)
+#     self.scales['yscrollcommand'] = self.scrollbar.set
+
+#     self.settings.mainloop()
     
-    self.scrollbar = tk.ttk.Scrollbar(self.settings, orient='vertical', command=self.scales.yview)# .yview is a problem yet
-    self.scrollbar.grid(row=0, column=1, sticky=tk.NS)
-    self.scales['yscrollcommand'] = self.scrollbar.set
+#   def getScaleValue(self):# command for getting values from scales
+#     value1 = self.scaleRowLength.get()# Var names are all wrong
+#     value2 = self.scaleColumnLenght.get()
+#     value3 = self.scalePopulation.get()
+#     value4 = self.scaleGeneration.get()
+#     self.settings.destroy()# closes the window
+#     #start the sim
 
-    self.settings.mainloop()
-    
-  def getScaleValue(self):# command for getting values from scales
-    value1 = self.scaleRowLength.get()# Var names are all wrong
-    value2 = self.scaleColumnLenght.get()
-    value3 = self.scalePopulation.get()
-    value4 = self.scaleGeneration.get()
-    self.settings.destroy()# closes the window
-    #start the sim
-
-# class Config:# maybe the class name is problematic
-#   def __init__(self,root):
-#     # root = tk.Tk() and root.mainloop() have to be directly in the specific class
-#     self.geometry = "800x600"
-#     root.geometry(self.geometry)
-#     root.resizable(width = False, heigth = False)
-    
-#     self.title = "Simulation"
-#     root.title(self.title)
-
-class Scale:
-  def __init__(self, root, scaleLow, scaleHigh):
-    self.scale = tk.ttk.Scale(root, from_ = scaleLow, to = scaleHigh)
-    self.scale.pack(expand = True) # not really a solution
-
-
-startpage = Startpage()
-startpage.run()
