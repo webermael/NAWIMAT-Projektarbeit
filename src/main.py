19993052ff550661618f347a9d1206cb867265d6
Mael Weber <113250761+webermael@users.noreply.github.com>
2024-09-19 11:45:23 +0200

    GUI (#19)
    * added GUI class
    
    * Rename GUI to GUI.py
    
    * Update GUI.py
    
    * added temporary title and geometry to GUI
    
    * Update GUI.py
    
    * added button1 and label1 to GUI.py
    
    * Added class Startpage/Settings , added scales
    
    * Added class Scale  and started on scrollbar
    
    * Added class Config and updated class Scale
    
    * import tkinter as tk (better late than never)
    
    * changed naming
    
    * changed start window to grid
    
    * changed 'Config' back to 'Settings'
    
    * renamed GUI.py -> gui.py
    
    * added Slider class + (basic) SettingsWindow
    
    * minimal changes (cleanup)
    
    * added start button to settings window
    
    * generating sliders from load_dict
    
    * changed from Slider to Box (sorry Max, thanks Yanis)
    
    * last commit with scrollbar
    
    * routine commit
    
    * fixed input num values (partially)
    
    * fixed input numbers
    
    * routine (if laptop burns down)
    
    * added saving screen
    
    * made looping values for Spinbox dependent on input file values (from 1/2 to 2)
    
    * made interface look less hideous
    
    * added interface values dictionary
    
    * changed settings window captions to be configurable through interface_values.py
    
    * added custom steps to interface values
    
    * fixed padding of settings window
    
    * removed numpy
    
    * removed redundant code
    
    * changed saving from selecting file to creating file
    
    * added relative path for template
    
    * removed slider class D:
    
    * added quitting
    
    * changed interface layout
    
    * changed main loop to Yanis' liking
    
    * fixed loop and saving
    
    * moved imports to one line
    
    * removed redundant code
    
    * reverted changes to simulation
    
    ---------
    
    Co-authored-by: Maximilian Bail <156407185+WanderJohnie@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,26 +1,52 @@
 from simulation import Simulation
+from utils.gui import LoadWindow, SettingsWindow, SaveWindow
 from utils.file_manager import FileManager
-import os.path
 
 def main():
     fm = FileManager()
 
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "template.json")
+    running = True
+    while running:
+        # --- LOAD FILE ---
+        load_window = LoadWindow()
+        load_window.run()
+        if load_window.pressed_quit:
+            break
+        load_dict = fm.load_file(load_window.loaded_file)
 
-    load_dict = fm.load_file(path)
+        same_file = True
+        while same_file:
+            same_file = False
+            # --- SETTINGS ---
+            settings_window = SettingsWindow(load_dict)
+            settings_window.run()
+            if settings_window.pressed_quit:
+                running = False
+                break
+            eyes_size = load_dict["population"]["organisms"]["eyes_size"]
+            load_dict["population"]["organisms"]["nn_layer_sizes"] = [2 * (eyes_size ** 2 + eyes_size) + 3, 
+                                                                        (1 + eyes_size) * 4, 
+                                                                        (1 + eyes_size) * 2, 
+                                                                        9]
+            if settings_window.start_simulation:
+                # --- SIMULATION ---
+                simulation = Simulation(load_dict)
+                simulation.evolve()
+                dict = fm.sim_to_dict(load_dict, simulation)
 
-    eyes_size = load_dict["population"]["organisms"]["eyes_size"]
-    load_dict["population"]["organisms"]["nn_layer_sizes"] = [2 * (eyes_size ** 2 + eyes_size) + 3, 
-                                                                (1 + eyes_size) * 4, 
-                                                                (1 + eyes_size) * 2, 
-                                                                9]
-    simulation = Simulation(load_dict)
+            # --- SAVING ---
+            save_window = SaveWindow()
+            save_window.run()
+            if save_window.pressed_quit:
+                running = False
+                break
+            elif save_window.saving:
+                fm.save_dict(save_window.save_file, dict)
+            elif save_window.keeping:
+                same_file = True
 
-    simulation.evolve()
-    
-    dict = fm.sim_to_dict(load_dict, simulation)
-    fm.save_dict("save.json", dict)
+            
+            
 
 if __name__ == "__main__":
     main()
